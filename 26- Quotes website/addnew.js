c99605386e24f6653aaa203f0f0dbb6c4c9b3264
SaraMecinovic1 <saramecinovic87@gmail.com>
2023-01-22 14:10:48 +0100

    domaci

--- 26- Quotes website/addnew.js
+++ 26- Quotes website/addnew.js
@@ -1,26 +1,42 @@
+// // !== (razlicit)
+// //prevu nmg da nadjem
+
+
+
+
+
+
 document.getElementById("addnew").addEventListener("click", function () {
-  var text = document.getElementById("qouteText").value;
-  var author = document.getElementById("qouteAuthor").value;
-  var source = document.getElementById("qouteSource").value;
+  var text = document.getElementById("quoteText").value;
+  var author = document.getElementById("quoteAuthor").value;
+  var source = document.getElementById("quoteSource").value;
 
   var newQuote = {
-    qouteText: text,
-    qouteAuthor: author,
-    qouteSource: source,
+    quoteText: text,
+    quoteAuthor: author,
+    quoteSource: source,
   };
+
   fetch("https://js-course-server.onrender.com/quotes/add-quote", {
     method: "POST",
     body: JSON.stringify(newQuote),
-    headers:{
-        accept: "application/json",
-        "Content-type":"application/json",
-
-    }
+    headers: {
+      accept: "application/json",
+      "Content-Type": "application/json",
+    },
   })
     .then(function (res) {
       return res.json();
     })
     .then(function (data) {
-      console.log("The quote is cretaed", data);
+      document.getElementById("quoteText").value="";
+      document.getElementById("quoteAuthor").value="";
+      document.getElementById("quoteSource").value="";
+
+      alert("Vas citat je uspesno dodat!");
     });
+
+
+    document.getElementById("poruka").innerHTML = "Go to the page: http://127.0.0.1:5500/26-%20Quotes%20website/index.html  to view quotes!"
 });
+
--- 26- Quotes website/script.js
+++ 26- Quotes website/script.js
@@ -1,11 +1,40 @@
 //ruta?
+
+var quotes = []; //svi idu tu
+var allQuotes = [];
+
+function renderQuotes() {
+  var parent = "";
+  var parent = document.getElementById("list");
+  quotes.forEach(function (item, index) {
+    var child = document.createElement("li");
+    child.textContent = item.quoteText + " -" + item.quoteAuthor;
+    parent.appendChild(child);
+  });
+}
+
 fetch("https://js-course-server.onrender.com/quotes/get-all-quotes")
   .then(function (response) {
-    return response.json();
+    //moze li ovo drugacije da se zove?
+    return response.json(); //rezultat da nam pretvori u json
   })
   .then(function (data) {
-    console.log("data", data);
+    //i ovo?
+    quotes = data;
+    renderQuotes();
   })
   .catch(function (err) {
     console.log("err", err);
   });
+
+document.getElementById("inp1").addEventListener("keyup", function () {
+  var searchValue = document.getElementById("inp1").value;
+  quotes = allQuotes.filter(function (item, index) {
+    if (item.quoteText) {
+      return item.quoteText.toLowerCase().includes(searchValue.toLowerCase());
+    } else {
+      return false;
+    }
+  });
+  renderQuotes();
+});
